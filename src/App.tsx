58c3be004b403dc1533b9292ce15e14bf24273ce
markvnukov <mark.vnukov@nrg-soft.com>
2024-04-22 09:26:11 +0200

    changed alert text

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       </div>
       {/*  */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+        <button onClick={() => WebApp.showAlert(`Hello Bro! Current count is ${count}`)}>
             Show Alert
         </button>
       </div>
